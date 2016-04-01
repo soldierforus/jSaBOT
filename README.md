7e48f490719e7e4d69f2e760521e4bf16ede74ea
Travis Bennett <tbennett3434@gmail.com>
2016-04-01 13:13:19 -0700

    v1.2 with savelog

---
+++ README.md
@@ -0,0 +1,38 @@
+Create a Twitter bot using Node.js and the Twitter API v1.1. The bot will automatically retweet in response to tweets with some particular hashtags. (https://goo.gl/4whEIt)
+
+Installation Instructions:
+
+1) Install Node.js
+    If you're using OS X or Windows, the best way to install Node.js is to use one of the installers from http://nodejs.org
+    npm is preferred.
+
+
+2) Create a twitter account, if you already have one, it may be used.
+
+3) Create a Twitter application
+    Go to apps.twitter.com, sign-in with your new Twitter account and create a Twitter application.
+
+    Give your application a name, description and put any URL in the website field. Keep the callback URL field blank, agree to the terms and submit the form to create your first Twitter application.
+
+    Once the Twitter application has been created, click the Keys and Access Tokens tab and click the
+    Create my Access Token button. Twitter will generate the Consumer Keys and Access tokens that we will need in a later step.
+
+4) Download the source code from github:
+  https://github.com/soldierforus/jSaBOT.git
+
+5) Edit the values below in the file "package.json"
+  Add the values for:
+   "keywords": ["#keywordToSearch","otherKeywords"]
+   "minutes": number of minutes between retweets
+   "consumer_key": "your key here"
+   "consumer_secret": "your key here"
+   "access_token": "your key here"
+   "access_token_secret": "your key here"
+
+
+6) In terminal, switch to the directory jSaBOT is installed and run jSabot.js:
+  node jsabot.js
+
+
+Licensed under http://creativecommons.org/licenses/by-sa/3.0
+Report error and bugs to travis.david.bennett@gmail.com - http://tdbennett.com
--- jSaBOT.js
+++ jSaBOT.js
@@ -1,25 +1,24 @@
 #!/usr/bin/env node
 /*
  * Bot.js : A Twitter bot that can retweet in response to the tweets matching multiple keywords.
- * Version 1.1.2
- 	Changes from original design:
-			-Added command line utilities
-	In upcoming releases:
-			-Add tracking log for time ran, total retweets, and retweet by keyword
  * Created by Travis Bennett (http://tdbennett.com @TravisDBennett.  Follow jSabot on twitter @javascriptSabot
  * Credit:
  		@StephenGFriend for getting me beck into #dev, all the time he spends answering my questions about code, and for helping me to debug nearly everything I write.
- 		@DebashisBarman for his tutorial on building a twitterbot his original design for which this bot was modeled after and aims to improve.
+ 		@DebashisBarman for his tutorial on building a twitterbot and his original design for which this bot was modeled.
  * License : http://creativecommons.org/licenses/by-sa/3.0
  */
+var Twit = require('twit');
+var fs = require('fs');
+var dataLogJSON = require('./dataLog.json');
+
+/* Read and parse dataLog.json */
+var packageJSON = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 /* Configure the Twitter API */
-var TWITTER_CONSUMER_KEY = '';
-var TWITTER_CONSUMER_SECRET = '';
-var TWITTER_ACCESS_TOKEN = '-6Skbhl3276YvDbN45NcYJeELRIZDMKVkrQNKx0a';
-var TWITTER_ACCESS_TOKEN_SECRET = '';
-
-var Twit = require('twit');
+var TWITTER_CONSUMER_KEY = packageJSON.consumer_key;
+var TWITTER_CONSUMER_SECRET = packageJSON.consumer_secret;
+var TWITTER_ACCESS_TOKEN = packageJSON.access_token;
+var TWITTER_ACCESS_TOKEN_SECRET = packageJSON.access_token_secret;
 
 var Bot = new Twit({
 	consumer_key: TWITTER_CONSUMER_KEY,
@@ -31,16 +30,50 @@ var Bot = new Twit({
 /* twitter phrase array counter */
 var counter = 0;
 
-/* Set Twitter search phrases */
-var twitterPhrase = ['TravisDBennett'];
+/* Get Twitter search phrases */
+var twitterPhrase = packageJSON.keywords;
+
+/* Count total retweets*/
+var retweetCounter = 0;
+var retweetTimer = 0;
+
+/* Save retweet log*/
+function saveLog(file, data){
+	fs.writeFile('dataLog.json', JSON.stringify(data), (err) => {
+		if (err) throw err;
+	});
+	console.log("\n"+"Log Saved")
+}
 
 /* Initialize the bot */
 function jSaBotInit() {
-  		console.log('jSaBOT v1.1 by |TRaViS BeNNeTT| initialized...\n\nNow retweeting the following keywords: \n' + twitterPhrase +'\n');
-
-/* Set first integer to minutes in between retweets */
+  console.log('jSaBOT v ' + packageJSON.version + ' by |Travis Bennett| initialized...\nLongest Session:\n  Ran for:   ' + dataLogJSON.time + ' minutes\n  Processed: ' + dataLogJSON.tweets + '  tweets\n' + '\nNow retweeting the following keywords: \n' + twitterPhrase +'\n');
 	Retweet();
-	setInterval(Retweet, 10*60*1000);
+	setInterval(Retweet, packageJSON.minutes*60*1000);
+}
+/* retweet count status every 30 minutes. */
+	setInterval(retweetCount, 30*60*1000);
+
+/* Count the number of retweets */
+function retweetCount() {
+	var dataLog = dataLogJSON
+
+	retweetTimer = retweetTimer + 30;
+	console.log("\njSaBOT has been running for " + retweetTimer + " mins\n" + "jSaBOT has processed: " + retweetCounter + " retweets\n");
+
+	function tweetRecord() {
+		if (dataLog.tweets < retweetCounter) {
+			dataLog.tweets = retweetCounter;
+		};
+	}
+	function timeRecord() {
+		if (dataLog.time < retweetTimer) {
+			dataLog.time = retweetTimer;
+		};
+	}
+	timeRecord();
+	tweetRecord();
+	saveLog('dataLog.json', dataLog);
 }
 
 /* BotRetweet() : To retweet the matching recent tweet */
@@ -77,11 +110,13 @@ function Retweet() {
 				else {
 					console.log('jSaBOT retweeted: ' + id.id + ' | ' + TWITTER_SEARCH_PHRASE);
 					counter++;
+					retweetCounter++;
 				}
 			}
 		}
 	}
 }
+
 
 /* Initiate the Bot */
 jSaBotInit();
